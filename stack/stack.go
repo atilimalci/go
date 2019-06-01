560de19c674194773acdc7dc0da3ec33e02ce557
ATILIM ALCI <alciatilim@gmail.com>
2019-06-01 13:00:51 +0300

    Stack using linked list

--- stack/stack.go
+++ stack/stack.go
@@ -10,42 +10,49 @@ type node struct {
 	next *node
 }
 
-func push(newData int, nodeList *node) *node {
-	newNode := &node{newData, nil}
-	newNode.next = nodeList
-	return newNode
+var top *node
+
+func push(newData int) {
+	var newNode = node{newData, nil}
+	newNode.next = top
+	top = &newNode
 }
 
-func pop(nodeList *node) *node {
-	if nodeList != nil {
-		nodeList = nodeList.next
-	}
-	return nodeList
-}
-
-func peek(nodeList *node) (int, error) {
-	if nodeList != nil {
-		return nodeList.data, nil
+func peek() (int, error) {
+	if top != nil {
+		return top.data, nil
 	}
 	return -1, errors.New("Empty list")
 }
 
+func pop() (int, error) {
+
+	if top != nil {
+		data := top.data
+		top = top.next
+		return data, nil
+	}
+	return -1, errors.New("Empty list")
+}
+
+func isEmpty() bool {
+	return top == nil
+}
+
 func main() {
-	a := &node{5, nil}
-	numberList := a
-	numberList = push(10, numberList)
-	numberList = push(15, numberList)
-	numberList = push(20, numberList)
-
-	data, _ := peek(numberList)
-	fmt.Println("Before pop ", data)
-
-	numberList = pop(numberList)
-
-	data, _ = peek(numberList)
+	fmt.Println("Is empty initially", isEmpty())
+	push(10)
+	fmt.Println("Is empty after push", isEmpty())
+	push(15)
+	push(20)
+	data, _ := peek()
+	fmt.Println("Before pop", data)
+	var popped, _ = pop()
+	fmt.Println("Popped data", popped)
+	data, _ = peek()
 	fmt.Println("After pop ", data)
 
-	for n := numberList; ; n = n.next {
+	for n := top; ; n = n.next {
 		fmt.Println(n.data)
 		if n.next == nil {
 			break
