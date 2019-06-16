0718aca228d2eef1bdd4c592e2f4bd635d56597e
ATILIM ALCI <alciatilim@gmail.com>
2019-06-17 00:36:02 +0300

    permutation

--- excercises/check_permutation.go
+++ excercises/check_permutation.go
@@ -5,15 +5,25 @@ import (
 	"strings"
 )
 
-func sortString(w string) string {
-	s := strings.Split(w, "")
-	sort.Strings(s)
-	return strings.Join(s, "")
+func sortString(w string) <-chan string {
+	r := make(chan string)
+	go func() {
+		s := strings.Split(w, "")
+		sort.Strings(s)
+		r <- strings.Join(s, "")
+		close(r)
+	}()
+	return r
 }
 
+// PermutationBySorting checks permutation by sorting strings
 func PermutationBySorting(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
-	return sortString(s) == sortString(t)
+
+	r1 := <-sortString(s)
+	r2 := <-sortString(t)
+
+	return r1 == r2
 }
