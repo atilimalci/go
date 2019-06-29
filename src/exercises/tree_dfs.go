3fba71a87a1488ff399d4bb90eaba0fc7ed2df7f
ATILIM ALCI <alciatilim@gmail.com>
2019-06-29 22:02:13 +0300

    Bfs recursive

--- src/exercises/tree_dfs.go
+++ src/exercises/tree_dfs.go
@@ -114,3 +114,25 @@ func (root *Node) bfs() string {
 
 	return r.String()
 }
+
+func (root *Node) bfsRecursive() string {
+	r := new(strings.Builder)
+	q := list.New()
+	q.PushBack(root)
+	bfsRecursive(q, r)
+	return r.String()
+}
+
+func bfsRecursive(q *list.List, r *strings.Builder) {
+	if q.Len() == 0 {
+		return
+	}
+	curr := pop(q)
+	if curr == nil {
+		return
+	}
+	r.WriteString(strconv.Itoa(curr.i))
+	q.PushBack(curr.left)
+	q.PushBack(curr.right)
+	bfsRecursive(q, r)
+}
